fb81b6d96d5d624ec1ebf18dddbaeeb11398059e
minna112-del <minna1128019@gmail.com>
2026-02-28 22:35:24 -0500

    Update Firebase configuration with new credentials
    Updated Firebase configuration with new credentials.

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,19 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// তোমার Firebase Console থেকে পাওয়া Config
 const firebaseConfig = {
-  apiKey: "....",
-  authDomain: "....",
-  projectId: "....",
-  storageBucket: "....",
-  messagingSenderId: "....",
-  appId: "....",
+  apiKey: "AIzaSyA9oKyU5LVspCQNtEbuPYsXe9yzvm63A8U",
+  authDomain: "lillahi-etimkhana.firebaseapp.com",
+  projectId: "lillahi-etimkhana",
+  storageBucket: "lillahi-etimkhana.firebasestorage.app",
+  messagingSenderId: "777373297360",
+  appId: "1:777373297360:web:61c04a82a1098204ea8cd5",
+  measurementId: "G-H8ZBFHJE6M"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
